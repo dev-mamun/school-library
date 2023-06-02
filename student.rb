2990a7ddfe9c7b5af99723d831c91d476f954cf3
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-06-03 00:42:39 +0600

    Set up associations

--- student.rb
+++ student.rb
@@ -1,10 +1,20 @@
 require_relative './person'
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :name, :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @classroom = nil
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 
   def play_hooky
--- test.rb
+++ test.rb
@@ -2,16 +2,49 @@ require_relative './person'
 require_relative './base_decorator'
 require_relative './capitalize_decorator'
 require_relative './trimmer_decorator'
+require_relative './student'
+require_relative './classroom'
+require_relative './rental'
+require_relative './book'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-trimmed_person = TrimmerDecorator.new(capitalized_person)
-trimmed_person.correct_name
+# Create some instances of the classes
+person = Person.new(25, 'John Doe' )
+student = Student.new(18, 'Class A', 'Jane Smith')
+classroom = Classroom.new('Class A')
+book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
 
-# Test the decorators
+# Create a rental between the student and the book
+rental = Rental.new('2023-05-20', book, student)
 
-puts "Original name: #{person.correct_name}"
-puts "Capitalized name: #{capitalized_person.correct_name}"
-puts "Trimmed name: #{trimmed_person.correct_name}"
+# Add a decorator to the person's name
+decorated_person = CapitalizeDecorator.new(person)
+
+# Trim the name of the decorated person
+trimmed_person = TrimmerDecorator.new(decorated_person)
+
+# Access the correct name of the trimmed person
+correct_name = trimmed_person.correct_name
+
+# Print the results
+puts "Person name: #{person.name}" # Output: John Doe
+puts "Student name: #{student.name}" # Output: Jane Smith
+puts "Classroom label: #{classroom.label}" # Output: Class A
+puts "Book title: #{book.title}" # Output: The Great Gatsby
+puts "Rental date: #{rental.date}" # Output: 2023-05-20
+puts "Decorated person name: #{decorated_person.correct_name}" # Output: JOHN DOE
+puts "Trimmed person name: #{trimmed_person.correct_name}" # Output: JOHN DOE
+
+puts "Correct name: #{correct_name}" # Output: JOHN DOE (trimmed to maximum of 10 characters)
+
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# capitalized_person.correct_name
+# trimmed_person = TrimmerDecorator.new(capitalized_person)
+# trimmed_person.correct_name
+#
+# # Test the decorators
+#
+# puts "Original name: #{person.correct_name}"
+# puts "Capitalized name: #{capitalized_person.correct_name}"
+# puts "Trimmed name: #{trimmed_person.correct_name}"
