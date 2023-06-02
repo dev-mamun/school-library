8ba7bfd1321e53237d5384927f81d0a1eb728d6c
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-06-03 02:04:58 +0600

    Fix linter

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 require_relative './person'
 class Student < Person
-  attr_accessor :name, :classroom, :parent_permission
+  attr_accessor :name, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
